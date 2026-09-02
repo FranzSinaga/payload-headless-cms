d9a4294171e4cf9ac94588bfb016240fdcdadeae
FranzSinaga <sinagafranz12@gmail.com>
2026-09-02 08:43:53 +0700

    Revert "feat: add baseline migration and wire migrate step into deploy"
    This reverts commit a67a13e6736d66a9f4bcd62f9b96c32f5da71227.

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260902_013415 from './20260902_013415';
-
-export const migrations = [
-  {
-    up: migration_20260902_013415.up,
-    down: migration_20260902_013415.down,
-    name: '20260902_013415'
-  },
-];
